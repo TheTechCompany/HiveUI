4515f18a9b065606021adb8101abaf9ca7583504
Connor Scott <connor@hexhive.io>
2022-03-25 13:45:54 +1300

    tidied up color-dot test

--- src/__tests__/ColorDot.test.tsx
+++ src/__tests__/ColorDot.test.tsx
@@ -4,18 +4,12 @@ import React from "react";
 import { render, fireEvent, waitFor, screen } from "@testing-library/react";
 import { ColorDot } from "..";
 
-import "@testing-library/jest-dom";
-
 describe("ColorDot", () => {
   test("renders at specified size", () => {
-    const { container } = render(<ColorDot size={5} color={"red"} />);
+    render(<ColorDot size={5} color={"red"} />);
 
-    const element = container.querySelector(".hive-color-dot");
+    const element = screen.getByRole("color-dot");
 
-    expect(element).toHaveStyle({
-      background: "red",
-      width: "5px",
-      height: "5px",
-    });
+    expect(element.style.width && element.style.height).toBe("5px");
   });
 });
