108ebf69ca5b2d02b431558e2be95f6af47f4cfb
Connor Scott <connor@hexhive.io>
2022-03-25 14:41:16 +1300

    tidied compomponent tests

--- src/__tests__/ColorDot.test.tsx
+++ src/__tests__/ColorDot.test.tsx
@@ -1,7 +1,7 @@
 // __tests__/ColorDot.test.ts
 import React from "react";
-
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { ColorDot } from "..";
 
 describe("ColorDot", () => {
--- src/__tests__/Kanban.test.tsx
+++ src/__tests__/Kanban.test.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Kanban } from "../components/Kanban/Kanban";
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 describe("KanBan", () => {
   test("Tests to come when component is working", () => {
