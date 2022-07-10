b33499902cf48899ed430c9f0ba4d4dd2d29e552
Ross Leitch <ross@hexhive.io>
2022-07-10 17:26:32 +1200

    Timeline and schedule styling

--- packages/hexhive-ui/src/components/ScheduleView/header/days.tsx
+++ packages/hexhive-ui/src/components/ScheduleView/header/days.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import moment from "moment";
 import React, { useContext } from "react";
 import { ScheduleViewContext } from "../context";
@@ -34,15 +34,16 @@ export const DayHeader : React.FC<DayHeaderProps> = (props) => {
                 alignItems: 'center',
                 justifyContent: 'center',
                 background: (currentDay == parseInt(renderTime(i, 'DD')) && currentMonth == parseInt(renderTime(i, 'MM'))) ? 'rgba(255, 255, 255, 0.2)' : '',
-                flexDirection: 'column'
-
+                flexDirection: 'column',
+                padding: '3px',
+                color: 'white'
               }}
               className={(currentDay == parseInt(renderTime(i, 'DD')) && currentMonth == parseInt(renderTime(i, 'MM'))) ? ' week-day-header week-day-header-current' : 'week-day-header'}>
               <Box>
-                {renderTime(i, 'ddd')}
+                <Typography>{renderTime(i, 'ddd')}</Typography>
               </Box>
               <Box>
-                {renderTime(i, 'DD/MM')}
+                <Typography>{renderTime(i, 'DD/MM')}</Typography>
               </Box>
             </Box>
           ))
