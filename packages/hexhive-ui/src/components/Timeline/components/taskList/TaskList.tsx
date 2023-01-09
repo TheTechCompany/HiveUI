a095fc1f9077d337416ed56a3f0880e425b8cc6e
Ross Leitch <ross@hexhive.io>
2023-01-09 17:08:13 +1300

    Sortable dnd in task-list

--- packages/hexhive-ui/src/components/Timeline/components/taskList/TaskList.tsx
+++ packages/hexhive-ui/src/components/Timeline/components/taskList/TaskList.tsx
@@ -6,6 +6,22 @@ import { TimelineContext } from '../../context';
 import Config from '../../helpers/config/Config';
 import { Task } from '../../types';
 import { TaskRow } from './TaskRow'
+
+import {
+  closestCenter,
+  useSensors,
+  useSensor,
+  DndContext,
+  PointerSensor
+} from '@dnd-kit/core'
+
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+  sortableKeyboardCoordinates
+} from '@dnd-kit/sortable'
+
+
 export class VerticalLine extends Component<any, any>{
   constructor(props: any) {
     super(props);
@@ -18,36 +34,40 @@ export class VerticalLine extends Component<any, any>{
 
 //Task Lis
 
-const TaskList : React.FC<any>  = (props) => {
+const TaskList: React.FC<any> = (props) => {
   const taskViewRef = useRef<HTMLDivElement>(null)
 
   const { tasks, itemHeight } = useContext(TimelineContext)
 
   const getContainerStyle = (rows: number) => {
-    let new_height = rows > 0 ? rows * ((itemHeight || 0) + 5 ): 10;
+    let new_height = rows > 0 ? rows * ((itemHeight || 0) + 5) : 10;
     return { height: new_height };
   }
+
+  const sensors = useSensors(
+    useSensor(PointerSensor)
+  )
 
   const renderTaskRow = (item: Task, i: number) => {
     // let result = [];
     // for (let i = props.startRow; i < props.endRow + 1; i++) {
     //   let item = data[i];
     //   if (!item) break;
-     return (
-        <TaskRow
-          key={item.id}
-          index={i}
-          item={item}
-          label={item.name}
-          top={i * ((itemHeight || 0) + 5)}
-          itemheight={itemHeight}
-          isSelected={props.selectedItem == item}
-          onUpdateTask={props.onUpdateTask}
-          onSelectItem={props.onSelectItem}
-          nonEditable={props.nonEditable}
-        />
-      );
-    
+    return (
+      <TaskRow
+        key={item.id}
+        index={i}
+        item={item}
+        label={item.name}
+        top={i * ((itemHeight || 0) + 5)}
+        itemheight={itemHeight}
+        isSelected={props.selectedItem == item}
+        onUpdateTask={props.onUpdateTask}
+        onSelectItem={props.onSelectItem}
+        nonEditable={props.nonEditable}
+      />
+    );
+
   }
 
   const doScroll = (e: any) => {
@@ -57,36 +77,46 @@ const TaskList : React.FC<any>  = (props) => {
 
   //Setup scroll listener for task list
   useEffect(() => {
-      taskViewRef.current?.addEventListener('scroll', doScroll)
+    taskViewRef.current?.addEventListener('scroll', doScroll)
 
-      return () => {
-        taskViewRef.current?.removeEventListener('scroll', doScroll)
-      }
+    return () => {
+      taskViewRef.current?.removeEventListener('scroll', doScroll)
+    }
   }, [])
 
-    const containerStyle = getContainerStyle((tasks || []).length);
+  const containerStyle = getContainerStyle((tasks || []).length);
 
-    return (
-      <Box className="timeLine-side">
-        <Box  
-          height={'60px'}
-          sx={{
-            height: '60px',
-            display: 'flex',
-            background: '#333',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          {/* <div>{Config.values.taskList.title.label}</div> */}
-        </Box>
-        <List 
-          className="timeLine-side-task-viewPort" 
-          onScroll={doScroll}>
-            {tasks?.map((datum, ix) => [renderTaskRow(datum, ix), <Divider />]).concat([renderTaskRow({}, tasks.length), <Divider />])}
-        </List>
+  return (
+    <Box className="timeLine-side">
+      <Box
+        height={'60px'}
+        sx={{
+          height: '60px',
+          display: 'flex',
+          background: '#333',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        {/* <div>{Config.values.taskList.title.label}</div> */}
       </Box>
-    );
-  
+      <DndContext
+        collisionDetection={closestCenter}
+        sensors={sensors}
+      >
+        <SortableContext
+          items={(tasks || []).map((x) => x.id || '')}
+          strategy={verticalListSortingStrategy}
+        >
+          <List
+            className="timeLine-side-task-viewPort"
+            onScroll={doScroll}>
+            {(tasks || []).map((datum, ix) => [renderTaskRow(datum, ix), <Divider />]).concat([renderTaskRow({}, (tasks || []).length), <Divider />])}
+          </List>
+        </SortableContext>
+      </DndContext>
+    </Box>
+  );
+
 }
 
 export default TaskList
--- packages/hexhive-ui/src/components/Timeline/components/taskList/TaskRow.tsx
+++ packages/hexhive-ui/src/components/Timeline/components/taskList/TaskRow.tsx
@@ -1,57 +1,90 @@
 import { Box, Typography } from "@mui/material";
-import React, { Component } from "react";
+import React, { useMemo } from "react";
 import Config from "../../helpers/config/Config";
+import { Task } from "../../types";
 import ContentEditable from "../common/ContentEditable";
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities';
 
-export class TaskRow extends Component<any, any> {
-    constructor(props: any) {
-      super(props);
-    }
-  
-    onChange = (value: any) => {
-      if (this.props.onUpdateTask) {
-        this.props.onUpdateTask(this.props.item, { name: value });
-      }
-    };
+export interface TaskRowProps {
 
-    get label() {
-      if(this.props.label?.length > 30){
-        return this.props.label?.substring(0, 30) + "...";
-      }
-      return this.props.label;
-    }
-  
-    render() {
-      return (
-        <Box
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            top: this.props.top,
-            paddingLeft: 12,
-            paddingRight: 12,
-            minHeight: this.props.itemheight + 4
-          }}
-          onClick={(e) => this.props.onSelectItem(this.props.item)}
-        >
-       
-        <div className="color-dot" style={{
-            width: 12, 
-            height: 12, 
-            borderRadius: 12, 
-            marginRight: 8,
-            background: this.props.item.color 
-        }} />
-  {/* this.props.nonEditable */}
-          {true ? (
-            <Box sx={{display: 'flex', whiteSpace: 'no-wrap', justifyContent: 'flex-start'}}  tabIndex={this.props.index} >
-              <Typography  textOverflow={'ellipsis'} fontSize={"10px"} >{this.label}</Typography>
-            </Box>
-          ) : (
-            <ContentEditable value={this.label} index={this.props.index} onChange={this.onChange} />
-          )}
-        </Box>
-      );
-    }
+  item: Task;
+  label?: string;
+
+  index: number;
+
+  isSelected?: boolean;
+  nonEditable?: boolean;
+
+  onUpdateTask?: (item: any, update: { name: string }) => void;
+  onSelectItem?: (item: any) => void;
+
+  top: number;
+  itemheight: number;
+
+}
+export const TaskRow: React.FC<TaskRowProps> = (props) => {
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition
+  } = useSortable({ id: props.item?.id || 'asdf' });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
   }
-  
+
+  const onChange = (value: any) => {
+    if (props.onUpdateTask) {
+      props.onUpdateTask?.(props.item, { name: value });
+    }
+  };
+
+  const label = useMemo(() => {
+    if ((props.label || '').length > 30) {
+      return props.label?.substring(0, 30) + "...";
+    }
+    return props.label;
+  }, [props.label])
+
+
+  return (
+    <Box
+      ref={setNodeRef}
+      {...attributes}
+      {...listeners}
+      style={{
+        ...style,
+        display: 'flex',
+        alignItems: 'center',
+        top: props.top,
+        paddingLeft: 12,
+        paddingRight: 12,
+        minHeight: props.itemheight + 4
+      }}
+      onClick={(e) => props.onSelectItem?.(props.item)}
+    >
+
+      <div className="color-dot" style={{
+        width: 12,
+        height: 12,
+        borderRadius: 12,
+        marginRight: 8,
+        background: props.item.color as string
+      }} />
+      {/* this.props.nonEditable */}
+      {true ? (
+        <Box sx={{ display: 'flex', whiteSpace: 'no-wrap', justifyContent: 'flex-start' }} tabIndex={props.index} >
+          <Typography textOverflow={'ellipsis'} fontSize={"10px"} >{label}</Typography>
+        </Box>
+      ) : (
+        <ContentEditable value={label} index={props.index} onChange={onChange} />
+      )}
+    </Box>
+  );
+
+}
